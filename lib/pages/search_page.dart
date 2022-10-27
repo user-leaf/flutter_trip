30cfef0789960027d199c281d0d426b4537966ad
KanBin <kanbin@jujin8.com>
2022-10-27 16:39:12 +0800

    [6-2]基于Http实现网络操作

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SearchPage extends StatefulWidget {
   @override
@@ -6,12 +9,58 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  String showResult = "";
+
+  var url =
+      Uri.http('www.devio.org', 'io/flutter_app/json/test_common_model.json');
+
+  Future<CommonModel> fetchPost() async {
+    final response = await http.get(url);
+    final result = jsonDecode(response.body);
+    return CommonModel.fromJson(result);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('搜索'),
+      appBar: AppBar(
+        title: Text('搜索页'),
+      ),
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              fetchPost().then((CommonModel value) {
+                setState(() {
+                  showResult =
+                      '请求结果: \nicon: ${value.icon} \ntitle: ${value.title} \nurl: ${value.url} \nstatusBarColor: ${value.statusBarColor} \nhideAppBar: ${value.hideAppBar}';
+                });
+              });
+            },
+            child: Text(
+              '点我',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Text(showResult),
+        ],
       ),
     );
+  }
+}
+
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel(
+      this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar);
+
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(json['icon'], json['title'], json['url'],
+        json['statusBarColor'], json['hideAppBar']);
   }
 }
